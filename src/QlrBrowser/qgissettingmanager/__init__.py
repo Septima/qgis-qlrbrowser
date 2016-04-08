3cc7e1c3a1906aa9c3e7bb18d02ee0a27e44642a
AsgerPetersen <asgerpetersen@gmail.com>
2016-04-08 12:05:01 +0200

    Fix qgissettingmanager import list bug

--- src/QlrBrowser/qgissettingmanager/__init__.py
+++ src/QlrBrowser/qgissettingmanager/__init__.py
@@ -2,6 +2,6 @@ from settingmanager import SettingManager
 from setting import Setting
 from settingdialog import SettingDialog
 
-from types import Bool, Color, Double. Integer, Stringlist, String
+from types import Bool, Color, Double, Integer, Stringlist, String
 
 
