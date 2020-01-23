5c7f0e5f2e6e9f8928f4be4ecbcd8dc33c7866f7
AsgerPetersen <asgerpetersen@gmail.com>
2020-01-23 10:04:49 +0100

    Add german translation
    Contributed by Stadtwerke München

---
+++ src/QlrBrowser/i18n/de.ts
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>ConfigDialog</name>
+    <message>
+        <location filename="settings_dialog.py" line="41"/>
+        <source>Base directory</source>
+        <translation>Verzeichnis</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="settings_dialog.ui" line="20"/>
+        <source>Qlr Browser settings</source>
+        <translation>Qlr Browser Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="settings_dialog.ui" line="33"/>
+        <source>Browse</source>
+        <translation>Durchsuchen</translation>
+    </message>
+    <message>
+        <location filename="settings_dialog.ui" line="48"/>
+        <source>Hide numeric prefixes in file names (formatted like: &apos;01~xxxx.qlr&apos; )</source>
+        <translation>Numerische Präfixe werden nicht angezeigt (Format: &apos;01~xxxx.qlr&apos; )</translation>
+    </message>
+    <message>
+        <location filename="settings_dialog.ui" line="58"/>
+        <source>Base directory:</source>
+        <translation>Verzeichnis:</translation>
+    </message>
+</context>
+<context>
+    <name>DockWidget</name>
+    <message>
+        <location filename="dockwidget.py" line="65"/>
+        <source>Filter</source>
+        <translation>Filter</translation>
+    </message>
+    <message>
+        <location filename="dockwidget.py" line="146"/>
+        <source>Configured1 base path has too many files (&gt; {})</source>
+        <translation>Konfiguriertes Verzeichnis beinhaltet zu viele Dateien (&gt; {})</translation>
+    </message>
+    <message>
+        <location filename="dockwidget.py" line="207"/>
+        <source>No base directory configured...</source>
+        <translation>Kein Verzeichnis konfiguriert...</translation>
+    </message>
+    <message>
+        <location filename="dockwidget.py" line="283"/>
+        <source>Qlr Browser Error</source>
+        <translation>Qlr Browser Fehler</translation>
+    </message>
+    <message>
+        <location filename="dockwidget.py" line="283"/>
+        <source>The selected path does not exist anymore. The Qlr Browser panel is being updated</source>
+        <translation>Der angegebene Pfad existiert nicht mehr. Das Qlr-Browser-Panel wird neu geladen</translation>
+    </message>
+</context>
+<context>
+    <name>QlrBrowser</name>
+    <message>
+        <location filename="qlrbrowser.py" line="67"/>
+        <source>QlrBrowser</source>
+        <translation>QlrBrowser</translation>
+    </message>
+    <message>
+        <location filename="qlrbrowser.py" line="72"/>
+        <source>&amp;Qlr Browser</source>
+        <translation>&amp;Qlr Browser</translation>
+    </message>
+</context>
+<context>
+    <name>QlrBrowserDockWidgetBase</name>
+    <message>
+        <location filename="dockwidget.ui" line="14"/>
+        <source>Qlr Browser</source>
+        <translation>Qlr Browser</translation>
+    </message>
+    <message>
+        <location filename="dockwidget.ui" line="54"/>
+        <source>Reload qlr files</source>
+        <translation>Lade qlr-Dateien neu</translation>
+    </message>
+    <message>
+        <location filename="dockwidget.ui" line="57"/>
+        <source>Reload</source>
+        <translation>Neuladen</translation>
+    </message>
+</context>
+<context>
+    <name>QlrManager</name>
+    <message>
+        <location filename="qlrmanager.py" line="129"/>
+        <source>Adding qlr-file to the layer panel</source>
+        <translation>Füge Layer aus der qlr-Datei hinzu</translation>
+    </message>
+    <message>
+        <location filename="qlrmanager.py" line="130"/>
+        <source>QlrBrowser</source>
+        <translation>QlrBrowser</translation>
+    </message>
+</context>
+</TS>
